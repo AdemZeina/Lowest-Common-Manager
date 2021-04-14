dca9efc8ca4fcb5c3e85c0bb07279977bb218484
Adem Zeina <azeina@geniustags.com>
2021-04-14 22:24:58 +0300

    Add project files.

---
+++ ConsoleApp4/BinaryTree.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp4
+{
+
+    // A Binary Tree node
+    public class Node
+    {
+        public string data;
+        public Node left, right;
+
+        public Node(string value)
+        {
+            data = value;
+            left = right = null;
+        }
+    }
+    public class BinaryTree
+    {
+
+        // Root of the Binary Tree
+        public Node root;
+
+        public Node findOutLCM(string e1, string e2)
+        {
+            return findOutLCM(root, e1, e2);
+        }
+
+        // This function returns pointer to LCM (Lowest Common Manager)
+        // of two given employyes e1 and e2. This
+        // function assumes that e1 and e2 are
+        // present in Binary Tree
+        Node findOutLCM(Node node, string e1, string e2)
+        {
+
+            // Base case
+            if (node == null)
+                return null;
+
+            // If either e1 or e2 matches with
+            // root's key, report the presence
+            // by returning root (Note that if
+            // a key is ancestor of other,
+            // then the ancestor key becomes LCM
+            if (node.data == e1 || node.data == e2)
+                return node;
+
+            // Look for keys in left and right subtrees
+            Node left_lcm = findOutLCM(node.left, e1, e2);
+            Node right_lcm = findOutLCM(node.right, e1, e2);
+
+            // If both of the above calls return Non-NULL,
+            // then one key is present in once subtree
+            // and other is present in other, So this
+            // node is the LCM
+            if (left_lcm != null && right_lcm != null)
+                return node;
+
+            // Otherwise check if left subtree or
+            // right subtree is LCM
+            return (left_lcm != null) ? left_lcm : right_lcm;
+        }
+    }
+}
---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using static ConsoleApp4.Program;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("write a function to find out the lowest common manager between two employees");
+
+
+            BinaryTree tree = new BinaryTree();
+
+            tree.root = new Node("VP");
+
+            tree.root.left = new Node("GM1");
+            
+            tree.root.left.left = new Node("TM1");
+            tree.root.left.left.left = new Node("IC1");
+            tree.root.left.left.right = new Node("IC2");
+
+            tree.root.left.right = new Node("TM2");
+            tree.root.left.right.left = new Node("IC3");
+   
+
+            tree.root.right = new Node("GM2");
+            tree.root.right.right = new Node("TM3");
+            tree.root.right.right.right = new Node("IC4");
+            
+            //tree.root.right = new Node("GM3");
+            //tree.root.right.right = new Node("TM4");
+
+
+            tree.root.Print();
+            Console.WriteLine();
+            Console.WriteLine("write a function to find out the lowest common manager between two employees");
+
+            Console.WriteLine("--------------------------------------");
+            Console.WriteLine("findOutLCM(IC1, IC2) = " +
+                              tree.findOutLCM("IC1", "IC2").data);
+            Console.WriteLine("findOutLCM(IC1, IC3) = " +
+                              tree.findOutLCM("IC1", "IC3").data);
+            Console.WriteLine("findOutLCM(IC1, IC4) = " +
+                              tree.findOutLCM("IC1", "IC4").data);
+            Console.WriteLine("findOutLCM(IC3, IC4) = " +
+                              tree.findOutLCM("IC3", "IC4").data);
+
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
+
+
+
